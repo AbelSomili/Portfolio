8a1247b09e5d1125575aeee8b9d79aab9e0908ec
Abel Somili <abelsomili4@gmail.com>
2025-11-21 09:13:37 +0200

    JS-Update
    Adds a navLinks object and a for each method

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ menu.addEveentListener('click', ()=> {
     navbar.classList.toggle('activate');
     menu.classList.toggle('bx-x');
 });
+
+
+// Close menu when clicking on a link
+const navLinks = document.querySelectorAll('.navbr a');
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=> {
+        navbar.classList.remove('active');
+        menu.classList.remove('bx-x');
+    });
+});
