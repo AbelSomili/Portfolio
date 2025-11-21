481bd38224c852fac8a6a2783f2c4e130d94541e
Abel Somili <abelsomili4@gmail.com>
2025-11-21 09:08:49 +0200

    JS Update
    Added a few changes to the menu and navbr objects that represent the menu icon and the navigation bar in the HTML document.

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbr');
+const menu = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbr');
 
-menu.onclick = ()=> {
+///  Toggle  ///
+menu.addEveentListener('click', ()=> {
+    navbar.classList.toggle('activate');
     menu.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
-
-window.onscroll = ()=>
-    menu.classList.remove('bx-x');
-    navbar.classList.remove('active');
+});
